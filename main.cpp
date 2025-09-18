628eb55cac6104e963dbbd55f877a327eae06902
Jeremias Salguero <JSalguero@axxonconsulting.com>
2025-09-18 09:14:50 -0300

    commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hola mundo\n";
+    cout << "Presiona ENTER para salir...";
+    cin.get();              // <- espera Enter
+    return 0;
+}
